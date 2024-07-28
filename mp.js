abff3e609a87650f958cc419d31a68b91b339013
flipflapflipflap <112861302+flipflapflipflap@users.noreply.github.com>
2024-07-28 12:56:02 -0400

    Update mp.js

--- mp.js
+++ mp.js
@@ -2317,13 +2317,10 @@ function countdown (element) {
 		}
 		else if (Month == 0) {
 			if (ChosenDays - dayoffset2 > Day) {
-						var dayDiff = Number(Day)+Number(dayoffset3);// + ((AugustHour+Number(Hour)) % 24);
-						var hourDiff = (AugustHour+Hour - ((AugustHour+Hour) % 24)*24);
-						var testing1 = AugustHour;
-						var testing2 = Hour;
-						var testing3 = (AugustHour+Number(Hour)) % 24;
+						var dayDiff = (Number(Day)+Number(dayoffset3));
+						var hourDiff = starttime - hour -1
 						fieldNameElement.innerHTML = "Invisible Storm in:";
-						cdtext = testing1 + ' : ' + testing1 + ' : ' + testing1 + ' : ' +dayDiff + ' : ' + hourDiff + ' : ' + Minute + ' : ' + Seconds;
+						cdtext = dayDiff + ' : ' + hourDiff + ' : ' + Minute + ' : ' + Seconds;
 					} else if (((31 - dayoffset1 == Day) && Hour < (24 - starttime)) || ((31 - dayoffset2 == Day) && Hour < (24 - starttime))) {
 						fieldNameElement.innerHTML = "";
 						cdtext = "SEIZON SENRYAKU";
