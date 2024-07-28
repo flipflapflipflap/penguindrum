51f994c5aa3b2cec24ac0a756ecfcb1f0a1558b3
flipflapflipflap <112861302+flipflapflipflap@users.noreply.github.com>
2024-07-28 16:45:44 -0400

    Update teamcolor.js

--- teamcolor.js
+++ teamcolor.js
@@ -1,4 +1,4 @@
-var teamList_4cc = ["chiemi","eriko","esmeralda","hibari","hikari","himari","kanba","keiju","kenzan","mario","masako","momoka","penguin1","penguin2","penguin3","ringo","sanetoshi","satoshi","shouma","yuri","beauty","cool","ginko","ginkobear","kaoru","konomi","kureha","lulu","lulubear","milne","mitsuko","reia","sexy","sumika","yurika"];
+var teamList_4cc = ["chiemi","eriko","esmeralda","hibari","hikari","himari","kanba","keiju","kenzan","mario","masako","momoka","penguin1","penguin2","penguin3","ringo","sanetoshi","satoshi","shouma","yuri","beauty","cool","ginko","ginkobear","kaoru","konomi","kureha","lulu","lulubear","milne","mitsuko","reia","sexy","sumika","yurika","akio","anthy","chuchu","dios","juri","kanae","keiko","kozue","kyouichi","mamiya","mari","miki","mitsuru","nanami","ruka","shiori","souji","tatsuya","touga","utena","wakaba"];
 
 
 function setTeamList(listOfTeams){
