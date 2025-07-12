a6caf257b22e7201791ff9a98240a2922971cb0c
flipflapflipflap <112861302+flipflapflipflap@users.noreply.github.com>
2025-07-12 06:24:46 -0400

    Update mp.js

--- mp.js
+++ mp.js
@@ -1544,12 +1544,12 @@ currenttimebtn = $('<button id="findtime" class="btn btn-xs btn-default" title="
 		}
 });
 
-$('<span id="maxusers" title="Maximum voyeurs">' + MAXUSERS + ' max voyeurs</span>')
+$('<span id="maxusers" title="Maximum lowlifes">' + MAXUSERS + ' max lowlifes</span>')
 	.appendTo("#chatheader")
 
 Callbacks.usercount = function(count) {
         CHANNEL.usercount = count;
-        var text = count + " voyeur";
+        var text = count + " lowlife";
         if(count != 1) {
             text += "s";
         }
@@ -1557,7 +1557,7 @@ Callbacks.usercount = function(count) {
 
 	if (MAXUSERS < count) {
 		MAXUSERS = count;
-		$("#maxusers").text(MAXUSERS + " max voyeurs");
+		$("#maxusers").text(MAXUSERS + " max lowlifes");
 		setOpt(CHANNEL.name + "_MAXUSERS" + (new Date().getFullYear()), MAXUSERS);
 	}
 };
